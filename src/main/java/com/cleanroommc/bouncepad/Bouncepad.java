d2d0c0effce4bcd59753adbc235d53c2f0b86d56
ShirosakiMio <852468399@qq.com>
2024-01-14 21:06:19 +0800

    Update Bouncepad.java

--- src/main/java/com/cleanroommc/bouncepad/Bouncepad.java
+++ src/main/java/com/cleanroommc/bouncepad/Bouncepad.java
@@ -62,7 +62,7 @@ public class Bouncepad {
         var imagineBreakerLibraryName = System.mapLibraryName("imaginebreaker");
         var osVersion = System.getProperty("os.version");
         if(osVersion != null && osVersion.toLowerCase().contains("android")) {
-            imagineBreakerLibraryName = "libimaginebreaker_android.so"
+            imagineBreakerLibraryName = "libimaginebreaker_android.so";
         }
         var imagineBreakerLibraryUrl = classLoader.getResource(imagineBreakerLibraryName);
         if (imagineBreakerLibraryUrl == null) {
